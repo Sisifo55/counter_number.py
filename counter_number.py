14e420467d0da211ed5f0486fc9fb92294ff3c83
Sergio Mariño <sergiusmaf@gmail.com>
2023-08-22 18:27:15 +0200

    Exersice FizzBuzz Python

---
+++ counter_number.py
@@ -0,0 +1,12 @@
+def counter (number = 101):
+    for num in range (1,number):
+        if num % 3 == 0 and num % 5 == 0:
+            print ("FizzBuzz")
+        elif num % 3 == 0:
+            print ("Fizz")
+        elif num % 5 == 0:
+            print ("Buzz")
+        else:
+            print (num)
+
+counter ()
